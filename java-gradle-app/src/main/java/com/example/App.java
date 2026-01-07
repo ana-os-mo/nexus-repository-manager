515871692c8ca229e69a1272060d23c7a1f4033e
Ana Osorio <88041760+ana-os-mo@users.noreply.github.com>
2026-01-07 11:37:13 -0500

    update gradle project (#2)

--- java-gradle-app/src/main/java/com/example/App.java
+++ java-gradle-app/src/main/java/com/example/App.java
@@ -2,25 +2,26 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@org.springframework.web.bind.annotation.RestController
 public class App {
 
-    public static void main(String[] args)
-    {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
-    public void init()
-    {
-        Logger log = LoggerFactory.getLogger(App.class);
+    public void init() {
         log.info("Java app started");
     }
 
+    @org.springframework.web.bind.annotation.GetMapping("/")
     public String getStatus() {
         return "OK";
     }
